ef90fdc24d3b07a2d13e2419e12ef90b19468385
o7-machinehum <ryan.cjw@gmail.com>
2023-08-01 10:48:14 -0700

    Cleaned up the firmware

--- fw/ff/ovrdrive.c
+++ fw/ff/ovrdrive.c
@@ -1,35 +1,73 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
+#include "ovrdrive.h"
+
+// #define INHIBIT
+#define DESTROY
+
+#define DWELL_TIME_US 50
+#define CHARGE_TIME_MS 125
+
+void pump(int charge_time) {
+    while(charge_time--) {
+        PORT_CP |= (1 << PIN_CP);
+        _delay_us(500);
+        PORT_CP &= ~(1 << PIN_CP);
+        _delay_us(500);
+    }
+
+    PORT_KILL |= (1 << PIN_KILL);
+    _delay_us(DWELL_TIME_US);
+    PORT_KILL &= ~(1 << PIN_KILL);
+}
+
+bool destroy_signal(void) {
+    return true;
+}
+
+void destroy(void) {
+    int n = 5;
+
+    if(destroy_signal()) {
+        while(n--) {
+            pump(CHARGE_TIME_MS);
+        }
+    }
+}
+
+void inhibit(void) {
+    if(destroy_signal()) {
+        PORT_INHIBIT |= (1 << PIN_INHIBIT);
+    }
+}
 
 int main(void) {
-    _delay_ms(5000);
-    DDRA  |=  (1 << PA0); // Charge Pump
-    DDRA  |=  (1 << PA2); // Inhibit Signal
-    DDRA  |=  (1 << PA3); // Kill signal
-    PORTA &= ~ (1 << PA3);
+    DDR_CP       |=  (1 << PIN_CP);
+    PORT_CP      &= ~(1 << PIN_CP);
 
-    DDRA  |=  (1 << PA7); // LED
-    PORTA &= ~(1 << PA2);
+    DDR_INHIBIT  |=  (1 << PIN_INHIBIT);
+    PORT_INHIBIT &= ~(1 << PIN_INHIBIT);
 
-    PORTA |= (1 << PA7);
+    DDR_KILL     |=  (1 << PIN_KILL);
+    PORT_KILL    &= ~(1 << PIN_KILL);
 
-    uint16_t timer = 5000;
-    // PORTA |= (1 << PA3);
-    while(1) {};
+    DDR_LED      |=  (1 << PIN_LED);
+
+#ifdef INHIBIT
+    inhibit();
+#endif
+
+#ifdef DESTROY
+    destroy();
+#endif
 
     while(1)
     {
-        PORTA |= (1 << PA0);
-        _delay_ms(1);
-        PORTA &= ~(1 << PA0);
-        _delay_ms(1);
-        timer--;
+        PORT_LED |= (1 << PIN_LED);
+        _delay_ms(1000);
+        PORT_LED &= ~(1 << PIN_LED);
+        _delay_ms(1000);
 
-        // if(!timer) {
-        //     PORTA |= (1 << PA3);
-        //     timer = 5000;
-        //     _delay_ms(1000);
-        //     PORTA &= ~(1 << PA3);
-        // }
     }
 }
---
+++ fw/ff/ovrdrive.h
@@ -0,0 +1,19 @@
+/* Charge Pump */
+#define PIN_CP PA0
+#define DDR_CP DDRA
+#define PORT_CP PORTA
+
+/* Flash Inhibit Signal */
+#define PIN_INHIBIT PA2
+#define DDR_INHIBIT DDRA
+#define PORT_INHIBIT PORTA
+
+/* Kill Signal */
+#define PIN_KILL  PA3
+#define DDR_KILL  DDRA
+#define PORT_KILL PORTA
+
+/* LED */
+#define PIN_LED   PA7
+#define DDR_LED   DDRA
+#define PORT_LED  PORTA
