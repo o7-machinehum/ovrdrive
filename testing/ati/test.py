e4bfbe89bcba78d2adee7bd8480bfb6f018d9ae4
o7-machinehum <ryan.cjw@gmail.com>
2024-05-06 14:29:16 +0200

    Working (single thread)

--- testing/ati/test.py
+++ testing/ati/test.py
@@ -3,33 +3,43 @@ import subprocess
 import platform
 import sys
 
-def test_devices(devices):
-    if not devices:
-        return
+def test_device(device):
+    result = subprocess.run(["sudo", "badblocks", "-b", "4096", "-svwt", "0xaa", device])
 
-    for device in devices:
-        result = subprocess.run(["badblocks", "-v", "-s", "-w", "-t", "0xaa", device])
-        if result.returncode != 0:
-            print(f"Error: {result.returncode} on Device: {device}") 
-        else:
-            print(f"{device}: OK")
+    if result.returncode != 0:
+        print(f"Error: {result.returncode} on Device: {device}") 
+    else:
+        print(f"{device}: OK")
 
-def format_devices(devices):
-    if not devices:
-        return
+def format_device(device):
+    print(f"Formatting {device} to FAT32...")
+    subprocess.run(["sudo", "mkfs.vfat", "-F", "32", device])
 
-    print("Formatting MSD devices to FAT32...")
-    for device in devices:
-        subprocess.run(["sudo", "mkfs.vfat", "-F", "32", "-n", "MSD_TEST", device])
+def unmount_device(device):
+    subprocess.run(["sudo", "umount", device])
+    subprocess.run(["sudo", "umount", device + "1"])
 
-    print("All MSD devices formatted to FAT32.")
+def mount_device(device):
+    folder = "mnt/"
+    subprocess.run(["mkdir", folder])
+    subprocess.run(["sudo", "mount", device, folder])
+
+def copy_payload(device):
+    folder = "mnt/"
+    subprocess.run(["sudo", "cp", "hello.txt", folder])
 
 def main():
     args = sys.argv[1:]
     test_answer = input(f"Do you want to test these devices: {args}? (y/n): ")
     if test_answer.lower() == "y":
-        test_devices(args)
-        format_devices(args)
+        subprocess.run(["mkdir", "mnt"])
+        for device in args:
+            unmount_device(device)
+            test_device(device)
+            format_device(device)
+            mount_device(device)
+            copy_payload(device)
+            unmount_device(device)
 
 if __name__ == "__main__":
     main()
